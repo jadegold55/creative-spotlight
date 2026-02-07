e0643b0729ee0788fbf974259b7d7f7dcbfc1861
jadegold55 <119713845+jadegold55@users.noreply.github.com>
2026-02-06 21:45:02 -0500

    Update artsandcrafts.py with modern discord.py code for Gallery Viewer

--- artsandcrafts.py
+++ artsandcrafts.py
@@ -1,82 +1,43 @@
 import discord
 from discord.ext import commands
-from os import environ as env
-from dotenv import load_dotenv
+from discord.ui import Button, View
 
-load_dotenv()
+intents = discord.Intents.default()
+client = commands.Bot(command_prefix="!", intents=intents)
 
-token = env["TOKEN"]
+class GalleryViewer(View):
+    def __init__(self, images):
+        super().__init__(timeout=None)  # Keep the view alive indefinitely
+        self.images = images
+        self.current_image_index = 0
 
+    @discord.ui.button(label="Previous", style=discord.ButtonStyle.primary)
+    async def previous(self, button: Button, interaction: discord.Interaction):
+        self.current_image_index = (self.current_image_index - 1) % len(self.images)
+        await self.update_image(interaction)
 
-class Bot(commands.Bot):
-    def __init__(self):
-        intents = discord.Intents.default()
-        intents.message_content = True
+    @discord.ui.button(label="Next", style=discord.ButtonStyle.primary)
+    async def next(self, button: Button, interaction: discord.Interaction):
+        self.current_image_index = (self.current_image_index + 1) % len(self.images)
+        await self.update_image(interaction)
 
-        super().__init__(
-            command_prefix=commands.when_mentioned_or("."), intents=intents
-        )
+    async def update_image(self, interaction: discord.Interaction):
+        embed = discord.Embed(title="Gallery Viewer")
+        embed.set_image(url=self.images[self.current_image_index])
+        await interaction.response.edit_message(embed=embed, view=self)
 
-    async def on_ready(self):
-        print(f"Logged in as {self.user} (ID: {self.user.id})")
-        print("-------")
+@client.slash_command(name="gallery", description="View the image gallery")
+async def gallery(interaction: discord.Interaction):
+    images = [
+        "url_to_image_1",  # Replace these with actual image URLs
+        "url_to_image_2",
+        "url_to_image_3",
+        "url_to_image_4",
+    ]
+    view = GalleryViewer(images)
 
+    embed = discord.Embed(title="Gallery Viewer")
+    embed.set_image(url=images[0])
+    await interaction.response.send_message(embed=embed, view=view)
 
-class Counter(discord.ui.View):
-    # number = 5
-    # this is the base from which the bots first comman will start
-    # what would be good to start with is figuring out how to make a next and previous button, these buttons will show a picture and
-    # a description of the picture.
-    # for a previous and next button, is making a button object better?
-    # or having two functions to define the interaction
-    # @discord.ui.button(label='0', style=discord.ButtonStyle.blurple)
-    # def counter(self, interaction: discord.Interaction, button: discord.ButtonStyle.blurple):
-    global number
-    number = 0
-
-    @discord.ui.button(label="prev", style=discord.ButtonStyle.red)
-    async def prev(self, interaction: discord.Interaction, button: discord.ui.Button):
-        global number
-        # number = int(button.label) if button.label else 0
-        # if number + 1 >= 5:
-        #     button.style = discord.ButtonStyle.green
-        #     button.disabled = True
-        # button.label = str(number + 1)
-        number -= 1
-        if number <= 0:
-            number = 0
-        # await Counter.counter(hey, interaction, button, str(Counter.number))
-        await interaction.response.edit_message(view=self, content=number)
-
-    @discord.ui.button(label="next", style=discord.ButtonStyle.grey)
-    async def next(self, interaction: discord.Interaction, button: discord.ui.Button):
-        global number
-        number += 1
-        if number >= 5:
-            number = 5
-
-        # await Counter.counter(, interaction, button, str(Counter.number))
-        await interaction.response.edit_message(view=self, content=number)
-
-        # Make sure to update the message with our updated selves
-
-
-class EphemeralSpotlight(discord.ui.View):
-    @discord.ui.button(label="click", style=discord.ButtonStyle.blurple)
-    async def receive(
-        self, interaction: discord.Interaction, button: discord.ui.Button
-    ):
-        await interaction.response.send_message(
-            "Enjoy!", view=Counter(), ephemeral=True
-        )
-
-
-bot = Bot()
-
-
-@bot.command()
-async def hi(ctx):
-    await ctx.send("hi", view=EphemeralSpotlight())
-
-
-bot.run(token=token)
+client.run('YOUR_BOT_TOKEN')  # Add your bot token here
