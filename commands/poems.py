04c17cc7bc64b56c0428c7af7e40565caeca84ff
jadegold55 <jadegold55@users.noreply.github.com>
2026-02-16 19:21:04 -0500

    fixed bug, added title and author functionality for random poem

--- commands/poems.py
+++ commands/poems.py
@@ -12,16 +12,23 @@ class poetry(commands.Cog):
         self.bot = bot
     #scraper will send an ephemeral message of a random poem to someone!
     @app_commands.command(name="poem", description="Get a random poem")
-    async def poem(self, interaction: discord.Interaction):
+    @app_commands.describe(author="Enter by Author",title="Enter by title")
+    async def poem(self, interaction: discord.Interaction, author: str=None, title: str=None):
         from scraping.randompoem import scrape
-        poem_data = scrape()
+        
+        poem_data = scrape(author=author,title=title)
+        
+        #find a workaround for users that ask for a title that has content length > 4096 
+
+        
         poem_embed = discord.Embed(
             title=poem_data['title'], description=f"By {poem_data['author']}\n\n{poem_data['content']}", color=discord.Color.blue())
-        if poem_data:
+        if poem_data:  #if not empty, don't want to be empty or might be logic in randompoem 
             
-            await interaction.response.send_message("Here's a random poem for you!", embed=poem_embed)
+            await interaction.response.send_message(f"Here's a random poem by {poem_data['author']}", embed=poem_embed)
         else:
             await interaction.response.send_message("Sorry, I couldn't fetch a poem at the moment.", ephemeral=True)
+    
 
 #will add  a daily poem event later, but for now this is just a test to make sure the command works and the bot can fetch and display a poem correctly. I will need to add some error handling and logging to make sure it runs smoothly, especially if i want to run it on a schedule.
 #commands to add: [poem of the day, poem search, upload poem]
--- scraping/randompoem.py
+++ scraping/randompoem.py
@@ -1,4 +1,5 @@
 
+import random
 import bs4 as BeautifulSoup
 import requests
 import logging
@@ -7,27 +8,36 @@ from datetime import datetime
 links = []
 # the point of this file might be more for having an event everyday which
 #the bot posts the new poem of the day from poetry foundation, but for now this is just a test to make sure i can scrape the page and get the poem links. I will need to add some error handling and logging to make sure it runs smoothly, especially if i want to run it on a schedule.
-def scrape():
-    url = "https://poetrydb.org/random"
+def scrape(author= None, title = None):
+    if author: 
+        url ="https://poetrydb.org/author,linecount/" + author + ";10"
+    elif title:
+        url = "https://poetrydb.org/title/" + title
+    else:
+        url = "https://poetrydb.org/linecount/10"
     try:
         response = requests.get(url)
         
         poems = response.json()
+
+        # while poem is empty or too long fetch one until we get a good one
+        #too many requests? 
         if isinstance(poems, list) and len(poems) > 0:
-            for poem in poems:
-                if len("\n".join(poem.get('lines', []))) < 4096:  # Discord embed description limit
-                    return {
+            print(len(poems))
+            poem = random.choice(poems)
+            return {
                         "title": poem.get('title', 'Unknown Title'),
                         "author": poem.get('author', 'Unknown Author'),
                         "content": "\n".join(poem.get('lines', []))
             }
+                    
         else:
             print("API returned an error or empty result:", poem)
-            return None
+            return None # don't want to return empty poem 
        
     except Exception as e:
         print(f"Error fetching from Poetrydb: {e.with_traceback(e.__traceback__)}")
-        return []
+        return [] # only happnes with badrequest, but want to return empty list so bot can handle it gracefully and not try to create an embed with empty data.
 
 
 
@@ -35,4 +45,5 @@ def scrape():
 if __name__ == "__main__":
     
     scraped_poems = scrape()
+    print(scraped_poems)
     
