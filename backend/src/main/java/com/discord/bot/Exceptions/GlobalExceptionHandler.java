0a851afbe0467f374c3490911344ce60e7dc3af4
Jade <cgold3555@gmail.com>
2026-02-21 23:11:56 -0500

    cleaned up null image code by just centralizing everything in service layer instead of controller

--- backend/src/main/java/com/discord/bot/Exceptions/GlobalExceptionHandler.java
+++ backend/src/main/java/com/discord/bot/Exceptions/GlobalExceptionHandler.java
@@ -5,8 +5,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import com.discord.bot.Exceptions.InvalidVote;
-
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
