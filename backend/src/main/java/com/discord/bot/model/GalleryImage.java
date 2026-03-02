dc9f61a832f03dee6131f50fb24fae338b5cf0fc
Jade <cgold3555@gmail.com>
2026-03-01 20:43:06 -0500

    fix

--- backend/src/main/java/com/discord/bot/model/GalleryImage.java
+++ backend/src/main/java/com/discord/bot/model/GalleryImage.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OnetoMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
