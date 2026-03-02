738291429ce88b2151eaf440639f1e0ba008d582
Jade <cgold3555@gmail.com>
2026-03-01 20:41:55 -0500

    fix

--- backend/src/main/java/com/discord/bot/model/GalleryImageVote.java
+++ backend/src/main/java/com/discord/bot/model/GalleryImageVote.java
@@ -2,7 +2,7 @@ package com.discord.bot.model;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OnetoMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
