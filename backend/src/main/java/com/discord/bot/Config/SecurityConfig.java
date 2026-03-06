e2f3b3d2c28ae730a8fa19144c5629056a58044a
Jade <cgold3555@gmail.com>
2026-03-06 17:47:05 -0500

    compment duplicate

--- backend/src/main/java/com/discord/bot/Config/SecurityConfig.java
+++ backend/src/main/java/com/discord/bot/Config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.discord.bot.Config;
 
 import java.security.Provider.Service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
